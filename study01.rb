f5f0c9859d5db3311e7ea72e2bebef9f6f2c20eb
maroHaru <134841618+maroHaru@users.noreply.github.com>
2024-02-24 07:57:46 +0000

    [Add] Ruby method learning

---
+++ study01.rb
@@ -0,0 +1,35 @@
+array = ["Ruby", "Rails", "javaScript", "Go", "PHP"]
+array << "HTML"
+puts array
+
+
+
+# string_1 = "DMMWEBCAMP"
+# string_2 = "DMMwEBCAMP"
+
+# puts string_1 == string_2
+
+# ring_1 = "DMMWEBCAMP"
+# ring_2 = "DMMwEBCAMP"
+
+# p ring_1 == ring_2
+
+# string = "DMMWEBCAMP, ruby"
+# p string.include?("WEb")
+
+# sample = "DMMWEBCAMP, ruby"
+# p sample.include?("WEB")
+
+# sample = "DMMWEBCAMP, ruby"
+# p sample.index("D")
+
+# sample = "DMMWEBCAMP, ruby"
+# p sample.index("A",1)
+
+# string = "DMMWEBCAMPDMMWEBCAMPDMMWEBCAMPDMMWEBCAMP"
+# p string.count("M")
+
+# string = "DMMWEBCAMP"
+# p string * 3
+
+
